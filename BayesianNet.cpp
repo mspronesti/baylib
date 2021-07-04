2b3c160bf16af874da07068a0dc3cb4655434cfe
Paolo Rabino <s283282@studenti.polito.it>
2021-07-04 19:03:16 +0200

    first commit

---
+++ BayesianNet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by paolo on 04/07/2021.
+//
+
+#include "BayesianNet.h"
---
+++ BayesianNet.h
@@ -0,0 +1,17 @@
+//
+// Created by paolo on 04/07/2021.
+//
+
+#ifndef GPUTEST_BAYESIANNET_H
+#define GPUTEST_BAYESIANNET_H
+
+
+class BayesianNet {
+private:
+
+public:
+
+};
+
+
+#endif //GPUTEST_BAYESIANNET_H
