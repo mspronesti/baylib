0810a21225f7fd0321629f41b0edac6618a401cc
paolotron <paolo.98.tron@gmail.com>
2021-07-23 20:18:04 +0200

    corrected cmake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 //
 // Created by paolo on 04/07/2021.
 //
-#include <fstream>
-#include <iostream>
-#include "parser/pugixml/pugixml.hpp"
-#include "old_graph/BayesianNet.h"
-#include "test/testFile.hpp"
+
 
 int main(){
-    bn::BayesianNet net("../xml_files/Coma.xdsl");
-    testFile();
+
 }
