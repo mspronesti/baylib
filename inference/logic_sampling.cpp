956225361975556f3ecca34cbf5ab69181d51823
paolotron <paolo.98.tron@gmail.com>
2021-07-24 11:42:15 +0200

    added prototype of node simulation

---
+++ inference/logic_sampling.cpp
@@ -0,0 +1,5 @@
+//
+// Created by paolo on 24/07/2021.
+//
+
+#include "logic_sampling.hpp"
--- inference/logic_sampling.hpp
+++ inference/logic_sampling.hpp
@@ -5,11 +5,20 @@
 #ifndef GPUTEST_LOGIC_SAMPLING_HPP
 #define GPUTEST_LOGIC_SAMPLING_HPP
 
+#define BOOST_COMPUTE_THREAD_SAFE
+
+#include <vector>
+#include <future>
+#include <boost/compute.hpp>
+
 namespace bn {
     template <typename T>
     class logic_sampling {
 
     };
+    template <typename T=float >
+    std::pair<int, int> simulate_node(std::vector<T> striped_cpt, std::vector<std::future<boost::compute::vector<int>>> parents_result, std::promise<boost::compute::vector<int>> result);
+
 }
 
 
