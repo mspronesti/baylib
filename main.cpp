b1b336b0c57b2a00219c49c654deb3a376109afd
PronElle <Massimiliano.Pronesti@gmail.com>
2021-07-26 22:21:41 +0200

    chore: refactor for namespaces

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
-//
-// Created by paolo on 04/07/2021.
-//
-
-#include "graphs/DAG.h"
+#include "graph/DAG.h"
 #include "inference/logic_sampling.hpp"
-#include "networks/BayesianNet.h"
+#include "network/bayesian_network.h"
 #include "tools/COW.h"
 #include "tools/thread_pool.hpp"
 #include "tools/VariableNodeMap.h"
 #include "probability/CPT.h"
 #include "parser/BNReader.h"
 #include "inference/logic_sampling.hpp"
+#include "network/bayesian_network.h"
 
 int main(){
-    std::shared_ptr<BayesianNetwork<double>> BN = std::make_shared<BayesianNetwork<double>>();
-    BNReader<double>().loadNetworkFromFile("../xml_files/Coma.xdsl", BN);
+    std::shared_ptr<bn::bayesian_network<double>> BN = std::make_shared<bn::bayesian_network<double>>();
+    BNReader<double>().loadNetworkFromFile("../examples/Coma.xdsl", BN);
 }
