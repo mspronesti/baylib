451a6b659f0229421592145bb2edd10dc6af5715
paolotron <paolo.98.tron@gmail.com>
2021-07-26 22:31:04 +0200

    feat(MonteCarloSim): added implementation for binary probability in parent node

--- inference/logic_sampling.hpp
+++ inference/logic_sampling.hpp
@@ -24,6 +24,16 @@ namespace bn {
         compute::command_queue queue;
         std::unique_ptr<compute::default_random_engine> rand_eng;
     public:
+        template<typename S>
+        void print_vec(compute::vector<S> &vec){
+            std::cout << "HERE\n";
+            std::vector<S> host_vec(vec.size());
+            compute::copy(vec.begin(), vec.end(), host_vec.begin(), queue);
+            for(T el: vec){
+                std::cout << el << ' ';
+            }
+            std::cout << '\n';
+        }
 
         logic_sampling() {
             this->device = compute::system::default_device();
@@ -39,17 +49,50 @@ namespace bn {
             this->rand_eng = std::make_unique<compute::default_random_engine>(queue);
         }
 
-        std::pair<int, int> simulate_node_agnostic(const std::vector<T>& striped_cpt, const std::vector<bcvec>& parents_result, bcvec& result){
-            if(parents_result.empty() && striped_cpt.size() == 2) {
+        std::pair<int, int>simulate_node_agnostic_debug(const std::vector<T>& striped_cpt, int possible_states, const std::vector<std::vector<int>>& parents_result, int dim=10){
+            std::vector<bcvec> gpu_par_result(parents_result.size());
+            bcvec result(dim, context);
+            std::transform(parents_result.begin(), parents_result.end(), gpu_par_result.begin(), [this](std::vector<int> par)->bcvec{
+                bcvec vec(par.size(), context);
+                boost::compute::copy(par.begin(), par.end(), vec.begin(), queue);
+                return vec;
+            });
+            return simulate_node_agnostic(striped_cpt, possible_states, gpu_par_result, result);
+        }
+
+        std::pair<int, int> simulate_node_agnostic(const std::vector<T>& striped_cpt, int possible_states, const std::vector<bcvec>& parents_result, bcvec& result){
+            if(parents_result.empty() && possible_states == 2) {
+                // Simple case with binary probabilities and no parents, solvable with simple bernoulli distribution
                 int sum = 0;
                 compute::bernoulli_distribution<T> distribution(striped_cpt[0]);
-                distribution.generate(result.begin(), result.end(), *this->rand_eng, queue);
+                distribution.generate(result.begin(), result.end(), *rand_eng, queue);
                 compute::reduce(result.begin(), result.end(), &sum, queue);
                 return std::pair<int, int>(sum, result.size() - sum);
+
             }
             else {
+                // Simple case with binary probabilities
                 compute::vector<T> device_cpt(striped_cpt.size(), context);
+                compute::vector<T> threshold_vec(result.size(), context);
+                bcvec index_vec(result.size(), context);
                 compute::copy(striped_cpt.begin(), striped_cpt.end(), device_cpt.begin(), queue);
+                for (int i = 0 ; i < parents_result.size() ; i++) {
+                    compute::function<int(int)> gen_index_first = compute::lambda::_1 * (1 << (i + 1));
+                    compute::function<int(int, int)> gen_index_and_sum = compute::lambda::_1 * (1 << (i + 1)) + compute::lambda::_2;
+                    if(i == 0)
+                        compute::transform(parents_result[i].begin(), parents_result[i].end(), index_vec.begin(), gen_index_first, queue);
+                    else
+                        compute::transform(parents_result[i].begin(), parents_result[i].end(), index_vec.begin(), index_vec.begin(), gen_index_and_sum, queue);
+                }
+                //compute::function<T(int)> get_threshold = device_cpt.begin() + compute::lambda::_1;
+                //compute::transform(index_vec.begin(), index_vec.end(), threshold_vec.begin(), get_threshold, queue);
+                print_vec(index_vec);
+                compute::gather(index_vec.begin(), index_vec.end(), device_cpt.begin(), threshold_vec.begin(), queue);
+                print_vec(device_cpt);
+                print_vec(threshold_vec);
+                compute::vector<T> random_vec(result.size(), context);
+                compute::uniform_real_distribution<T> distribution(0.0f, 1.0f);
+                distribution.generate(random_vec.begin(), random_vec.end(), *rand_eng, queue);
             }
 
             return std::pair<int, int>();
--- main.cpp
+++ main.cpp
@@ -17,14 +17,19 @@ void test_simulation_source(){
     std::vector<float> probabilities = {0.2, 0.8};
     std::vector<bn::bcvec> parents = {};
     bn::bcvec result(1000);
-    auto res = ls.simulate_node_agnostic(probabilities, parents, result);
+    auto res = ls.simulate_node_agnostic(probabilities, 2, parents, result);
+    std::cout << res.first << " " << res.second << "\n";
+}
+void test_simulation_child(){
+    auto ls = bn::logic_sampling<float>();
+    std::vector<float> probabilities = {0.2, 0.8, 0.5, 0.5};
+    std::vector<int> parent_res = {0, 0, 1, 1, 0};
+    std::vector<std::vector<int>> parents_vec(1);
+    parents_vec[0] = std::move(parent_res);
+    auto res = ls.simulate_node_agnostic_debug(probabilities, 2, parents_vec, 5);
     std::cout << res.first << " " << res.second << "\n";
 }
 
-void test_simulation_child(){
-
-}
-
 int main(){
-    test_simulation_source();
+    test_simulation_child();
 }
