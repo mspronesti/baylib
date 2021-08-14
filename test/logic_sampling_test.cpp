9d80c22c1c22cc0fb58859d14fdc7d44ece980fe
paolotron <paolo.98.tron@gmail.com>
2021-08-14 19:05:05 +0200

    feat: added logic_sampling_test.cpp

--- test/logic_sampling_test.cpp
+++ test/logic_sampling_test.cpp
@@ -2,4 +2,134 @@
 // Created by elle on 05/08/21.
 //
 
+#include <gtest/gtest.h>
+#include <baylib/network/bayesian_network.hpp>
+#include <baylib/network/bayesian_utils.hpp>
+#include <baylib/parser/net_parser.hpp>
+#include <baylib/inference/logic_sampling.hpp>
 // TODO: to be implemented
+
+#define TOLERANCE .1
+
+class logic_sampling_tests : public ::testing::Test {
+protected:
+
+    bn::bayesian_network<float> net1;
+    bn::bayesian_network<float> net2;
+    bn::bayesian_network<float> net3;
+
+    void SetUp() override{
+        //https://repo.bayesfusion.com/network/permalink?net=Small+BNs%2FComa.xdsl
+        net1 = bn::net_parser<float>().load_from_xdsl("../../test/xdsl/Coma.xdsl");
+        //https://repo.bayesfusion.com/network/permalink?net=Small+BNs%2FVentureBNExpanded.xdsl
+        net2 = bn::net_parser<float>().load_from_xdsl("../../test/xdsl/VentureBNExpanded.xdsl");
+        //https://repo.bayesfusion.com/network/permalink?net=Small+BNs%2FCredit.xdsl
+        net3 = bn::net_parser<float>().load_from_xdsl("../../test/xdsl/Credit.xdsl");
+    }
+
+};
+
+TEST_F(logic_sampling_tests, Coma_errors){
+    auto fun = [this](){bn::logic_sampling<float> el = bn::logic_sampling<float>(net1);};
+    bn::condition cond{};
+    cond.add("IncrSerCal", 0);
+    cond.add("BrainTumor", 0);
+    net1.set_variable_probability("Coma", 0, cond, .99);
+    ASSERT_ANY_THROW(fun());
+    net1.set_variable_probability("Coma", 0, cond, .001);
+    ASSERT_ANY_THROW(fun());
+}
+
+
+TEST_F(logic_sampling_tests, big_bang_Coma){
+    bn::logic_sampling<float> sampling(net1);
+    auto result = sampling.compute_network_marginal_probabilities(5*2^20, 10000, 2);
+
+    ASSERT_NEAR(result[net1.index_of("MetastCancer")][0], .2, TOLERANCE);
+    ASSERT_NEAR(result[net1.index_of("MetastCancer")][1], .8, TOLERANCE);
+
+    ASSERT_NEAR(result[net1.index_of("IncrSerCal")][0], .32, TOLERANCE);
+    ASSERT_NEAR(result[net1.index_of("IncrSerCal")][1], .68, TOLERANCE);
+
+    ASSERT_NEAR(result[net1.index_of("Coma")][0], .32, TOLERANCE);
+    ASSERT_NEAR(result[net1.index_of("Coma")][1], .68, TOLERANCE);
+
+    ASSERT_NEAR(result[net1.index_of("BrainTumor")][0], .8, TOLERANCE);
+    ASSERT_NEAR(result[net1.index_of("BrainTumor")][1], .92, TOLERANCE);
+
+    ASSERT_NEAR(result[net1.index_of("SevHeadaches")][0], .62, TOLERANCE);
+    ASSERT_NEAR(result[net1.index_of("SevHeadaches")][1], .38, TOLERANCE);
+}
+
+TEST_F(logic_sampling_tests, big_bang_VentureBNExpanded){
+    bn::logic_sampling<float> sampling(net2);
+    auto result = sampling.compute_network_marginal_probabilities(5*2^20, 10000, 2);
+
+    ASSERT_NEAR(result[net2.index_of("Success")][0], .2, TOLERANCE);
+    ASSERT_NEAR(result[net2.index_of("Success")][1], .8, TOLERANCE);
+
+    ASSERT_NEAR(result[net2.index_of("Economy")][0], .2, TOLERANCE);
+    ASSERT_NEAR(result[net2.index_of("Economy")][1], .7, TOLERANCE);
+    ASSERT_NEAR(result[net2.index_of("Economy")][2], .1, TOLERANCE);
+
+    ASSERT_NEAR(result[net2.index_of("Forecast")][0], .23, TOLERANCE);
+    ASSERT_NEAR(result[net2.index_of("Forecast")][1], .3, TOLERANCE);
+    ASSERT_NEAR(result[net2.index_of("Forecast")][2], .47, TOLERANCE);
+}
+
+TEST_F(logic_sampling_tests, big_bang_Credit){
+    bn::logic_sampling<float> sampling(net3);
+    auto result = sampling.compute_network_marginal_probabilities(5*2^20, 10000, 2);
+
+    ASSERT_NEAR(result[net3.index_of("PaymentHistory")][0], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("PaymentHistory")][1], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("PaymentHistory")][2], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("PaymentHistory")][3], .25, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("WorkHistory")][0], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("WorkHistory")][1], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("WorkHistory")][2], .25, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("WorkHistory")][3], .25, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Reliability")][0], .45, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Reliability")][1], .55, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Debit")][0], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Debit")][1], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Debit")][2], .33, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Income")][0], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Income")][1], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Income")][2], .33, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("RatioDebInc")][0], .47, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("RatioDebInc")][1], .53, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Assets")][0], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Assets")][1], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Assets")][2], .33, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Worth")][0], .59, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Worth")][1], .22, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Worth")][2], .19, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Profession")][0], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Profession")][1], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Profession")][2], .33, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("FutureIncome")][0], .68, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("FutureIncome")][1], .32, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("Age")][0], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Age")][1], .33, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("Age")][2], .33, TOLERANCE);
+
+    ASSERT_NEAR(result[net3.index_of("CreditWorthiness")][0], .54, TOLERANCE);
+    ASSERT_NEAR(result[net3.index_of("CreditWorthiness")][1], .46, TOLERANCE);
+}
+
+
+int main(int argc, char** argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
